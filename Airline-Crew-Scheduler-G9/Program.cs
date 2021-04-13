2886e73e70fea8c2fab73fb4144888b1694a2b9a
BuildTools <unconfigured@null.spigotmc.org>
2021-04-12 22:19:17 -0500

    added Program class with Main method

---
+++ Airline-Crew-Scheduler-G9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Airline_Crew_Scheduler_G9
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Todo: write program
+            Console.WriteLine("Hello World!");
+            
+
+        }
+    }
+}
