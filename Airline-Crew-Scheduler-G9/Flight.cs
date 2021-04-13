53f930e6c1fd776e361175ebc8463241964211de
Sam Kirchner <skirchner3@huskers.unl.edu>
2021-04-13 16:15:42 -0500

    added some Classes: Airport, Aircraft, Employee, Crew

---
+++ Airline-Crew-Scheduler-G9/Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline_Crew_Scheduler_G9
+{
+    class Flight
+    {
+        //todo: add takeoff/ touchdown times
+        public int FlightNumber { get; set; }
+        public Aircraft FlightAircraft { get; set; }
+        public Airport OriginAirport { get; set; }
+        public Airport DestinationAirport { get; set; }
+        public Crew FlightCrew { get; set; } 
+
+        public Flight()
+        {
+            //TODO: make constructor
+        }
+    }
+}
